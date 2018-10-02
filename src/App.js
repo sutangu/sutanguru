ecfcdacfc4a313b8afc1703e3d5faf2a48bfe581
sutangu <sutangu@gmail.com>
2018-10-02 23:46:47 +0600

    add noscript version of the site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import wakatime from './images/wakatime-120.png';
 import github from './images/github-icon.svg';
+import lastfm from './images/last-fm-3-256.jpg';
 
 class App extends Component {
     render() {
@@ -13,13 +14,18 @@ class App extends Component {
                 </p>
                 <ul className='list'>
                     <li>
-                        <a href='https://wakatime.com/@sutangu'>
+                        <a href='https://wakatime.com/@sutangu' target='_blank'>
                             <img className='icon' src={ wakatime } alt='wakatime' height='25px'/>
                         </a>
                     </li>
                     <li>
-                        <a href='https://github.com/sutangu'>
+                        <a href='https://github.com/sutangu' target='_blank'>
                             <img className='icon' src={ github } alt='github' height='25px'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://www.last.fm/ru/user/Cu-Real' target='_blank'>
+                            <img className='icon' src={ lastfm } alt='github' height='25px'/>
                         </a>
                     </li>
                 </ul>
