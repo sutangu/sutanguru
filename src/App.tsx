f12d869bafec82eac6499ecbb94aeb3c279f4aa3
sutangu <sutangu@gmail.com>
2019-11-12 05:45:32 +0300

    add link to myshows, temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -26,17 +26,25 @@ class App extends Component {
                     <h1>{tr('main.sutangu')}</h1>
                     <h2>{tr('main.subtitle')}</h2>
                     <h3>{tr('main.h3')}</h3>
+
                     <div className='email'>
                         <a href="mailto:me@sutangu.ru">me@sutangu.ru</a>
                     </div>
+
                     <a href="https://stackoverflow.com/users/5266871/alexander-shtang" target='_blank' rel="noopener noreferrer">
                         <img src="https://stackoverflow.com/users/flair/5266871.png?theme=clean" width="208" height="58"
                              alt="profile for Alexander Shtang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
                              title="profile for Alexander Shtang at Stack Overflow, Q&amp;A for professional and enthusiast programmers"/>
                     </a>
+
                     <p className="intro">
                         Here will be website 'bout sutangu.
                     </p>
+
+                    <a href="https://myshows.me/Enclave88">
+                        <img src="https://u.myshows.me/u/23370.png" alt='https://myshows.me/Enclave88' />
+                    </a>
+
                     <ul className='list'>
                         <li>
                             <a className='social-icon' href='https://wakatime.com/@sutangu' target='_blank' rel="noopener noreferrer">
