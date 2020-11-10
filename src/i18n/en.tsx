c431dac90705e20d5ca7db78143d31279e1eb536
sutangu <sutangu@gmail.com>
2020-11-10 15:39:27 +0300

    add link to my ALS page

--- src/i18n/en.tsx
+++ src/i18n/en.tsx
@@ -1,7 +1,8 @@
 export default {
   main: {
     sutangu: 'sutangu',
-    subtitle: 'Opensourcænimous',
+    subtitle: 'Opensourcænimous, currently working at ',
+    subtitle_ALS: 'Art. Lebedev Studio',
     h3: 'Things that are impossible just take longer.',
     portal_launch_expect: 'The site is expected to launch ~ in 2025'
   }
--- src/i18n/ru.tsx
+++ src/i18n/ru.tsx
@@ -1,7 +1,8 @@
 export default {
   main: {
     sutangu: 'сутангу',
-    subtitle: 'Opensourcænimous',
+    subtitle: 'Opensourcænimous, работаю в ',
+    subtitle_ALS: 'Студии Артемия Лебедева',
     h3: 'Невозможное лишь занимает больше времени.',
     portal_launch_expect: 'Запуск ожидается ~ в 2025 году'
   }
